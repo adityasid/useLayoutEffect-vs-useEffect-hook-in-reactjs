bba78b007ad13e332f57bf45d75691d88d5d2c0d
AdityaSid <adityasid786@gmail.com>
2022-01-12 04:46:42 +0530

    useLayoutEffect-vs-useEffect

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function UseEffect() {
+
+    const [show, setShow] = useState(false);
+    const popupRef = useRef()
+    const buttonRef = useRef()
+
+    useEffect(() => {
+        if (popupRef.current == null || buttonRef.current == null) return
+        const { bottom } = buttonRef.current.getBoundingClientRect()
+        popupRef.current.style.top = `${bottom + 25}px`;
+    }, [show])
+
     return (
-        <div>
-            UseEffect
-        </div>
+        <>
+            <button ref={buttonRef} onClick={() => setShow(prev => !prev)}>
+                Click Here
+            </button>
+            {show && (
+                <div style={{ position: "absolute" }} ref={popupRef}>
+                    This is a popup
+                </div>
+            )}
+        </>
     )
 }
 
--- src/components/UseLayoutEffect.js
+++ src/components/UseLayoutEffect.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useLayoutEffect, useRef } from 'react'
 
 function UseLayoutEffect() {
+
+    const [show, setShow] = useState(false);
+    const popupRef = useRef()
+    const buttonRef = useRef()
+
+    useLayoutEffect(() => {
+        if (popupRef.current == null || buttonRef.current == null) return
+        const { bottom } = buttonRef.current.getBoundingClientRect()
+        popupRef.current.style.top = `${bottom + 25}px`;
+    }, [show])
+
     return (
-        <div>
-            UseLayoutEffect
-        </div>
+        <>
+            <button ref={buttonRef} onClick={() => setShow(prev => !prev)}>
+                Click Here
+            </button>
+            {show && (
+                <div style={{ position: "absolute" }} ref={popupRef}>
+                    This is a popup
+                </div>
+            )}
+        </>
     )
 }
 
